256210c57a3a7652e07a04f0c3fe8ead5bcb4046
Vasyl Posmit <vasyl.posmit@gmail.com>
2016-03-31 17:49:08 +0300

    add specs for getBMI()

--- src/app/common/default.controller.js
+++ src/app/common/default.controller.js
@@ -13,80 +13,80 @@
                       $http,
                       hotkeys
                       ) {
-      "ngInject";
-      /*jshint validthis: true*/
-      var self = this;
-      self.state = 'default';
-      self.message = '';
+    "ngInject";
+    /*jshint validthis: true*/
+    var self = this;
+    self.state = 'default';
+    self.message = '';
 
-      self.toggleSideNav = sidenavService.toggleList;
-      self.openLeftMenu = sidenavService.openLeftMenu;
-      self.closeSidenav = sidenavService.closeSidenav;
+    self.toggleSideNav = sidenavService.toggleList;
+    self.openLeftMenu = sidenavService.openLeftMenu;
+    self.closeSidenav = sidenavService.closeSidenav;
 
-      self.user = userService.user;
-      self.data = sectionsService.data[self.state];
-      self.calculate = calculate;
-      self.getBMI = getBMI;
-      activate();
+    self.user = userService.user;
+    self.data = sectionsService.data[self.state];
+    self.calculate = calculate;
+    self.getBMI = getBMI;
+    activate();
 
-      function activate() {
-        self.closeSidenav();
-        if (userService.firstLaunch) {
-          $http.get('/assets/vasylData.json').then(function(result){
-            if (_.isObject(result.data)){
-              userService.user = result.data;
-            } else {
-              userService.user = result;
-            }
-            calculate();
-            userService.firstLaunch = false;
-          });
-        }
-      }
-
-      function calculate(){
-        self.user = userService.getUser();
-        self.data = sectionsService.getData()[self.state];
-        userService.setStoredUser(self.user);
-      }
-
-      function getBMI(event) {
-        calculate();
-        self.message = 'You body mass index is ' + self.user.BMI + '. That means you are ';
-        if (self.user.BMI<19) {
-          self.message += 'slim.';
-        } else if (self.user.BMI >= 19 && self.user.BMI <= 25){
-          self.message += 'fit.';
-        } else if (self.user.BMI<19){
-          self.message += 'overweight.';
-        }
-        self.message += ' Also consider that your Basal metabolic rate is '+
-           self.user.BMR +'. That means you need ' + self.user.BMR +
-           ' calories per day just to maintain basic body functions.';
-
-        var confirm = $mdDialog.confirm({
-          title : 'Information about your shape',
-          textContent : self.message,
-          ariaLabel : 'Lucky day',
-          targetEvent: event,
-          clickOutsideToClose: true,
-          ok : 'Display',
-          cancel:'Hide'
-        });
-
-        $mdDialog.show(confirm).then( function(ok){},
-         function(cancel) {
-          self.message = 'You decided to keep your information hidden.';
-        });
-      }
-
-      hotkeys.add({
-        combo: ['enter'],
-        description: 'Calculate recommendations',
-        callback: function() {
+    function activate() {
+      self.closeSidenav();
+      if (userService.firstLaunch) {
+        $http.get('/assets/vasylData.json').then(function(result){
+          if (_.isObject(result.data)){
+            userService.user = result.data;
+          } else {
+            userService.user = result;
+          }
           calculate();
-        }
+          userService.firstLaunch = false;
+        });
+      }
+    }
+
+    function calculate(){
+      self.user = userService.getUser();
+      self.data = sectionsService.getData()[self.state];
+      userService.setStoredUser(self.user);
+    }
+
+    function getBMI(event) {
+      calculate();
+      self.message = 'You body mass index is ' + self.user.BMI + '. That means you are ';
+      if (self.user.BMI<19) {
+        self.message += 'slim.';
+      } else if (self.user.BMI >= 19 && self.user.BMI <= 25){
+        self.message += 'fit.';
+      } else if (self.user.BMI > 25){
+        self.message += 'overweight.';
+      }
+      self.message += ' Also consider that your Basal metabolic rate is '+
+         self.user.BMR +'. That means you need ' + self.user.BMR +
+         ' calories per day just to maintain basic body functions.';
+
+      var confirm = $mdDialog.confirm({
+        title : 'Information about your shape',
+        textContent : self.message,
+        ariaLabel : 'Lucky day',
+        targetEvent: event,
+        clickOutsideToClose: true,
+        ok : 'Display',
+        cancel:'Hide'
       });
+
+      $mdDialog.show(confirm).then( function(ok){},
+       function(cancel) {
+        self.message = 'You decided to keep your information hidden.';
+      });
+    }
+
+    hotkeys.add({
+      combo: ['enter'],
+      description: 'Calculate recommendations',
+      callback: function() {
+        calculate();
+      }
+    });
 
   }
 })();
--- src/app/common/default.controller.spec.js
+++ src/app/common/default.controller.spec.js
@@ -9,24 +9,31 @@
       module('healthGuide');
 
       inject( function(
-                        $compile, $controller,
-                        $rootScope, $localForage,
-                        _$mdSidenav_, _sectionsService_,
-                        _userService_){
+                        $compile,
+                        $controller,
+                        $rootScope,
+                        $localForage,
+                        $httpBackend,
+                        _$mdSidenav_,
+                        _sectionsService_,
+                        _userService_
+                        ){
         userService = _userService_;
         sectionsService = _sectionsService_;
         localForage = $localForage;
         scope = $rootScope.$new();
-
         dataKey = 'nutrition';
-        controller = function(){
+        controller = (function(){
           controller = $controller('defaultController', {$scope: scope});
           controller.state = dataKey;
           controller.data = sectionsService.data[dataKey];
           return controller;
-        }();
+        }());
         $compile('<md-sidenav md-component-id="left"></md-sidenav>')(scope);
         sidenav = _$mdSidenav_('left');
+
+        $httpBackend.when('GET', '/assets/vasylData.json')
+          .respond(userService.user);
       });
     });
 
@@ -46,7 +53,7 @@
       });
 
       it('firstLaunch is true', function() {
-        expect(userService.firstLaunch).toBeTruthy();
+        expect(userService.firstLaunch).toEqual(true);
       });
 
       it('user object has mostly null values', function() {
@@ -122,6 +129,59 @@
         controller.closeSidenav();
         expect(sidenav.isOpen()).toEqual(false);
       });
+
+      describe('getBMI()', function() {
+
+        it('should include BMI into controller.message ', function() {
+          controller.user.nutrition.weight = 40;
+          controller.user.nutrition.height = 150;
+          userService.firstLaunch = false;
+          controller.getBMI();
+          scope.$digest();
+          expect(controller.message).toContain('17.8');
+        });
+
+        it('should display BMR value into controller.message', function() {
+          controller.user.nutrition.weight = 40;
+          controller.user.nutrition.height = 150;
+          controller.user.age = 25;
+          controller.user.isMale = false;
+          controller.getBMI();
+          scope.$digest();
+          expect(controller.message).toContain('1208');
+        });
+
+        it('should displays "slim" message if BMI < 19', function() {
+          controller.user.nutrition.weight = 40;
+          controller.user.nutrition.height = 150;
+          controller.getBMI();
+          scope.$digest();
+          expect(controller.message).toContain('slim');
+          expect(controller.message).not.toContain('fit');
+          expect(controller.message).not.toContain('overweight');
+        });
+
+        it('should displays "fit" message if 19 < BMI < 25', function() {
+          controller.user.nutrition.weight = 50;
+          controller.user.nutrition.height = 150;
+          controller.getBMI();
+          scope.$digest();
+          expect(controller.message).not.toContain('slim');
+          expect(controller.message).toContain('fit');
+          expect(controller.message).not.toContain('overweight');
+        });
+
+        it('should displays "overweight" message if BMI > 25', function() {
+          controller.user.nutrition.weight = 70;
+          controller.user.nutrition.height = 150;
+          controller.getBMI();
+          scope.$digest();
+          expect(controller.message).not.toContain('slim');
+          expect(controller.message).not.toContain('fit');
+          expect(controller.message).toContain('overweight');
+        });
+      });
+
     });
   });
 })();
